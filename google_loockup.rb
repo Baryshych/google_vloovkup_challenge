2318342e1603968d53fcbea86aeb1e1476d5fb17
luka <luka.baryshych@quartesian.com>
2020-04-30 13:37:42 +0300

    lint

--- google_loockup.rb
+++ google_loockup.rb
@@ -1,15 +1,14 @@
-require "google_drive"
+require 'google_drive'
 
 TABLE_KEY = '1BLBd3hZEyyNXo3KocHEAuo5klbZAd0b8NLB6-pve1Uo'.freeze
-SESSION = GoogleDrive::Session.from_service_account_key(
-    "key.json")
+SESSION = GoogleDrive::Session.from_service_account_key('key.json')
 
 @ws = SESSION.spreadsheet_by_key(TABLE_KEY).worksheets[0]
 
 
 def loockup(key)
- row = @ws.rows.find { |r| r[0] == key }
- p row ? row[1] : 'Nothing found'
+  row = @ws.rows.find { |r| r[0] == key }
+  p row ? row[1] : 'Nothing found'
 end
 
- loockup(ARGV[0])
+loockup ARGV[0]
