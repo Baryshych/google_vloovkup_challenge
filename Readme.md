8a2c81f6cf72417d870bff5f22ef0d09bf520b8d
luka <luka.baryshych@quartesian.com>
2020-04-30 13:27:33 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,3 @@
+Файл с конфигурацей ключа сервисного аккаунта кладется в корень с именем key.rb \n
+Алиас - alias google_vloockup='ruby <b>\*полный путь*</b>/google_loockup.rb' \n
+Ссылка на редактируемый файл - https://docs.google.com/spreadsheets/d/1BLBd3hZEyyNXo3KocHEAuo5klbZAd0b8NLB6-pve1Uo/edit?usp=sharing
---
+++ google_loockup.rb
@@ -0,0 +1,15 @@
+require "google_drive"
+
+TABLE_KEY = '1BLBd3hZEyyNXo3KocHEAuo5klbZAd0b8NLB6-pve1Uo'.freeze
+SESSION = GoogleDrive::Session.from_service_account_key(
+    "key.json")
+
+@ws = SESSION.spreadsheet_by_key(TABLE_KEY).worksheets[0]
+
+
+def loockup(key)
+ row = @ws.rows.find { |r| r[0] == key }
+ p row[1]
+end
+
+ARGV.each { |option| loockup(option) }
